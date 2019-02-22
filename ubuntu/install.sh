90be730dc01a031be88e0080dbe722555e864798
762293490@qq.com <762293490@qq.com>
2019-02-22 23:22:13 +0800

    update address

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -14,8 +14,8 @@ virtualenv -p /usr/bin/python3 ~/.virtualenv/py3
 
 source ~/.virtualenv/py3/bin/activate
 
-wget -qO- https://raw.githubusercontent.com/jokernotgod/vimrc/master/setup.sh | sh -x
+wget -qO- https://raw.githubusercontent.com/formatedd/vimrc/master/setup.sh | sh -x
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-wget https://raw.githubusercontent.com/jokernotgod/questions/master/ubuntu/Schminitz.zsh-theme -P ~/.oh-my-zsh/custom/themes
+wget https://raw.githubusercontent.com/formateddd/questions/master/ubuntu/Schminitz.zsh-theme -P ~/.oh-my-zsh/custom/themes
