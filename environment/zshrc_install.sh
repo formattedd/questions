856dcc3a1de3c428c5b80576be44fd42a6e73404
MacPro <762293490@qq.com>
2019-06-03 14:37:12 +0800

    install ohmyzsh

--- environment/zshrc_install.sh
+++ environment/zshrc_install.sh
@@ -1,95 +1,73 @@
-#!/bin/bash
+#!/bin/sh
 
-# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-JudgeZshrcExists(){
-    if [ ! -d "~/.oh-my-zsh" ]; then
-        cp ~/.zshrc ~/.zshrc.orig
-        mv -f ~/.oh-my-zsh ~/oh-my-zsh-old
+JudgeZshrcExist(){
+    if [ ! -d "~/.zshrc" ]; then
+        mv -f ~/.zshrc ~/.zshrc_back
     fi
 }
 
-InstallGitZsh(){
+InstallZsh(){
     echo "installing..."
     if which apt-get >/dev/null; then
-        sudo apt-get install -y git zsh
+        sudo apt-get install -y zsh
     elif which brew >/dev/null;then
-        brew install git zsh
+        brew install zsh
     elif which yum >/dev/null;then
-        sudo yum install -y git zsh
+        sudo yum install zsh
     else
-        echo "Can not judge system"
+        echo "Does not support system version"
     fi
 }
 
-DownloadZshrc(){
+InstallOhMyZsh(){
     git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+    echo "which zsh"
+    chsh -s /bin/zsh
+}
 
+InstallThemesPlugins(){
+    wget https://raw.githubusercontent.com/formateddd/questions/master/environment/Schminitz.zsh-theme -P ~/.oh-my-zsh/custom/themes/
     git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
-    wget https://raw.githubusercontent.com/formateddd/questions/master/environment/Schminitz.zsh-theme -P ~/.oh-my-zsh/custom/themes
-    echo "      Download zsh and plugins finish"
 }
 
-UpdateZshrc(){
-    echo "
-DISABLE_AUTO_UPDATE="true"
+WriteZshrc(){
+    echo "# Path to your oh-my-zsh installation.
+    export ZSH=$HOME/.oh-my-zsh
 
-# source ~/.virtualenv/py2/bin/activate
-source ~/.virtualenv/py3/bin/activate
+    # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+    ZSH_THEME='Schminitz'
 
-alias gs='gst'
-alias vi='vim'
+    # Add wisely, as too many plugins slow down shell startup.
+    plugins=(
+        zsh-autosuggestions 
+        git)
 
-alias dp='docker ps -a'
-alias dk='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
-alias dr='docker rmi $(docker images -f "dangling=true" -q)'
-# docker rmi $(docker images | grep "none" | awk '{print $3}')
-alias dc='docker-compose'
-alias di='docker images'
-alias dir='docker rmi'
+    source $ZSH/oh-my-zsh.sh
 
-alias getpass='openssl rand -base64 20'
-alias www='python -m http.server 8000'
-alias getip='curl ipinfo.io/ip'
+    DISABLE_AUTO_UPDATE='true'
 
-function ccat() {
-    local style="monokai"
-    if [ $# -eq 0 ]; then
-        pygmentize -P style=$style -P tabsize=4 -f terminal256 -g
-    else
-        for NAME in $@; do
-            pygmentize -P style=$style -P tabsize=4 -f terminal256 -g "$NAME"
-        done
-    fi
+    alias scpr='rsync -Pzv --rsh=ssh'
+    alias getpass='openssl rand -base64 20'
+    alias www='ifconfig && python -m http.server 8000'
+    alias getip='curl ipinfo.io/ip'
+
+    # alias gs='gst'
+
+    # alias dp='docker ps -a'
+    # alias dk='docker stop \$(docker ps -a -q) && docker rm \$(docker ps -a -q)'
+    # alias dr='docker rmi \$(docker images -f "dangling=true" -q)'
+    # alias dc='docker-compose'
+    # alias di='docker images'
+    # alias dir='docker rmi'
+
+    # python virtualenv 
+    # source ~/.virtualenv/py3/bin/activate" >  ~/.zshrc
+    source ~/.zshrc
 }
 
-plugins=(
-zsh-autosuggestions
-git)
-ZSH_THEME=\"Schminitz\"
-
-export ZSH=$HOME/.oh-my-zsh
-source $ZSH/oh-my-zsh.sh
-
-# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
-# 
-# export GOROOT=/usr/local/Cellar/go/1.11.1/libexec
-# export GOPATH=/Users/jinlong/Codes/go
-# export PATH=$PATH:/usr/local/Cellar/go/1.11.1/bin/:$GOPATH/bin
-# export GOBIN=$GOPATH/bin " > ~/.zshrc
-
-    # cat ~/.oh-my-zsh/templates/zshrc.zsh-template | while read line
-    # do
-    #     echo $line >> ~/.zshrc
-    # done
-
-}
-
-JudgeZshrcExists
-InstallGitZsh
-DownloadZshrc
-UpdateZshrc
-
-echo "install finish !"
+JudgeZshrcExist
+InstallZsh
+InstallOhMyZsh
+InstallThemesPlugins
+WriteZshrc
