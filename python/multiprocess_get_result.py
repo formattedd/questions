b24bd7d17998e1ff0c36f360def173119c6c9868
MacPro <762293490@qq.com>
2019-07-05 15:11:09 +0800

    add func

--- python/multiprocess_get_result.py
+++ python/multiprocess_get_result.py
@@ -1,19 +1,32 @@
 import multiprocessing
 
 
-def func(msg):
+def f(msg):
+    print(msg)
     return multiprocessing.current_process().name + '-' + msg
 
 
-if __name__ == "__main__":
+def func1():
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
     results = []
     for i in range(10):
         msg = "hello %d" % (i)
-        results.append(pool.apply_async(func, (msg, )))
+        results.append(pool.apply_async(f, (msg, )))
     pool.close()
     pool.join()
     print("Sub-process(es) done.")
 
     for res in results:
         print(res.get())
+
+
+def func2():
+    from multiprocessing import Pool
+    # with Pool(5) as p:
+    with Pool(processes=multiprocessing.cpu_count()) as p:
+        print(p.map(f, [str(i) for i in range(9)]))
+
+
+if __name__ == "__main__":
+    # func1()
+    func2()
