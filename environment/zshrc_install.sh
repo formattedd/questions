d4b2667fe51d7643f673223f1a72111b363501dd
MacPro <zxc76229@163.com>
2019-08-21 11:05:31 +0800

    update getip

--- environment/zshrc_install.sh
+++ environment/zshrc_install.sh
@@ -62,7 +62,7 @@ DISABLE_AUTO_UPDATE='true'
 alias scpr='rsync -Pzv --rsh=ssh'
 alias getpass='openssl rand -base64 20'
 alias www='ifconfig && python -m http.server 8000'
-alias getip='curl ipinfo.io/ip'
+alias ip='curl -L tool.lu/ip'
 alias setproxy='export ALL_PROXY=socks5://127.0.0.1:1080'
 alias unsetproxy='unset ALL_PROXY'
 
