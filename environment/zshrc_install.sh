afadc52e1af6e9df47a934ce7d6c542244ebca19
MacPro <ycm76229@gmail.com>
2019-11-21 09:06:05 +0800

    update unsetproxy

--- environment/zshrc_install.sh
+++ environment/zshrc_install.sh
@@ -70,7 +70,7 @@ alias www='ifconfig && python -m http.server 8000'
 # alias getip='curl -L tool.lu/ip'
 alias getip='curl http://cip.cc'
 alias setproxy='export ALL_PROXY=socks5://127.0.0.1:1080'
-alias unsetproxy='unset ALL_PROXY'
+alias unsetproxy='unset ALL_PROXY; unset http_proxy; unset https_proxy'
 
 alias gs='git status'
 alias ga='git add'
