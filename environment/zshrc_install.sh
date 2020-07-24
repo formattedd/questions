bf43b8ab0938c2462c1af7c37b2e6fd49d74d00d
ycm76229@gmail.com <ycm76229@gmail.com>
2020-07-24 15:23:00 +0800

    update zsh config

--- environment/zshrc_install.sh
+++ environment/zshrc_install.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 JudgeZshrcExist(){
-    if [ ! -d "~/.zshrc" ]; then
-        mv -f ~/.zshrc ~/.zshrc_back
-    fi
+    [[ ! -f ~/.zshrc ]] || mv ~/.zshrc ~/.zshrc_back
+    # if [ ! -d "~/.zshrc" ]; then
+    #     mv -f ~/.zshrc ~/.zshrc_back
+    # fi
 }
 
 InstallZsh(){
     echo "installing..."
     if which apt-get >/dev/null; then
-	sudo apt-get update -y && sudo apt-get install -y zsh git
+        sudo apt-get update -y && sudo apt-get install -y zsh git
     elif which brew >/dev/null;then
         brew install zsh git
     elif which yum >/dev/null;then
         sudo yum install zsh git -y
+    elif which pacman >/dev/null;then
+        sudo pacman -S zsh git
     else
         echo "Does not support system version"
     fi
@@ -26,81 +29,13 @@ InstallOhMyZsh(){
 }
 
 InstallThemesPlugins(){
-    wget https://raw.githubusercontent.com/formateddd/questions/master/environment/Schminitz.zsh-theme -P ~/.oh-my-zsh/themes/
     git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 }
 
 WriteZshrc(){
-
-echo "if [ -d "~/.pyenv/py3" ]; then
-    source ~/.pyenv/py3/bin/activate
-else
-    echo "python virtualenv not existed"
-fi
-
-if type nvim > /dev/null 2>&1; then
-    alias vi='nvim'
-fi
-# alias vi='mvim -v'
-
-bindkey '^n' end-of-line
-
-# Path to your oh-my-zsh installation.
-export ZSH=$HOME/.oh-my-zsh
-DISABLE_AUTO_UPDATE='true'
-# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME='Schminitz'
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(
-  zsh-autosuggestions 
-  zsh-syntax-highlighting
-)
-source $HOME/.oh-my-zsh/oh-my-zsh.sh
-
-export LC_ALL="zh_CN.UTF-8"
-
-# pip install better_exceptions
-# export BETTER_EXCEPTIONS=1
-# export FLASK_APP=app.py
-# export FLASK_ENV=development
-# export GOPROXY=https://goproxy.io
-# export GOPATH="$HOME/projects/go"
-# export PATH="$GOPATH/bin:$PATH"
-
-alias scpr='rsync -Pzv --rsh=ssh'
-# alias getpass='openssl rand -base64 20'
-alias getpass='openssl rand -hex 32'
-alias www='ifconfig && python -m http.server 8000'
-# alias getip='curl ipinfo.io/ip'
-# alias getip='curl -L tool.lu/ip'
-alias getip='curl http://cip.cc'
-alias wt='curl wttr.in/nanjing'
-alias setproxy='export ALL_PROXY=socks5://127.0.0.1:1080'
-alias unsetproxy='unset ALL_PROXY; unset http_proxy; unset https_proxy'
-
-alias gc='git clone'
-alias gs='git status'
-alias ga='git add'
-alias gcmsg='git commit -m'
-alias gco='git checkout'
-alias gd='git diff'
-alias gl='git pull'
-alias gp='git push'
-
-alias dp='docker ps -a'
-# alias dk='docker stop \$(docker ps -a -q) && docker rm \$(docker ps -a -q)'
-alias dk='docker rm \$(docker ps -a -q)'
-alias dr='docker rmi \$(docker images --filter "dangling=true" -q --no-trunc)'
-alias dc='docker-compose'
-alias di='docker images'
-alias dir='docker rmi'
-
-# git -C \"\$(brew --repo)\" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
-# git -C \"\$(brew --repo homebrew/core)\" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
-# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
-
-" >  ~/.zshrc
+    wget https://raw.githubusercontent.com/formateddd/questions/master/environment/zshrc -P ~/.zshrc
 }
 
 JudgeZshrcExist
