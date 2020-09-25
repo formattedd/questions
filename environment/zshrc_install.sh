5a40a69799ec146a20c845b0cc70306425682275
ycm76229 <ycm76229@gmail.com>
2020-09-26 00:36:32 +0800

    update reuse Schminitz

--- environment/zshrc_install.sh
+++ environment/zshrc_install.sh
@@ -29,13 +29,13 @@ InstallOhMyZsh(){
 }
 
 InstallThemesPlugins(){
-    git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 }
 
 WriteZshrc(){
-    wget https://raw.githubusercontent.com/formateddd/questions/master/environment/zshrc ~/.zshrc
+    wget https://raw.githubusercontent.com/formateddd/questions/master/environment/zshrc $HOME/.zshrc
 }
 
 JudgeZshrcExist
